ec070dbd13ae142eb5cd141605ce39c18313087d
Germy Louis <glouis@imobile3.com>
2022-01-19 00:26:29 -0700

    Add unit tests for network calls.

--- EmployeeDirectory/app/src/test/java/EmployeeRepoImplTest.kt
+++ EmployeeDirectory/app/src/test/java/EmployeeRepoImplTest.kt
@@ -1,40 +1,64 @@
 import com.example.employeedirectory.constants.Constants
 import com.example.employeedirectory.data.datasources.EmployeeDataSource
 import com.example.employeedirectory.data.datasources.RetrofitInstance
+import com.example.employeedirectory.data.datasources.api.NetworkHelper.safeApiCall
+import com.example.employeedirectory.data.datasources.api.ResultWrapper
 import com.example.employeedirectory.data.models.Employee
 import com.example.employeedirectory.data.repos.EmployeeRepoImpl
 import io.mockk.MockKAnnotations.init
 import io.mockk.impl.annotations.MockK
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
-import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 
-
+@ExperimentalCoroutinesApi
 class EmployeeRepoImplTest {
 
     @MockK
     lateinit var employeeList: ArrayList<Employee>
 
-    lateinit var employeeRepoImpl: EmployeeRepoImpl
+    private lateinit var employeeRepoImpl: EmployeeRepoImpl
+
+    private val dispatcher = StandardTestDispatcher()
 
     @Before
     fun setUp() {
         init(this)
-        employeeRepoImpl = EmployeeRepoImpl(
-            RetrofitInstance.getRetrofitInstance(Constants.EMPLOYEES_URL)?.create(
-                EmployeeDataSource::class.java
-            )
-        )
+        Dispatchers.setMain(dispatcher)
     }
 
     @Test
-    fun getEmployees() = runBlocking {
-        employeeList = employeeRepoImpl.getEmployees()?.value?.employees as ArrayList<Employee>
+    fun getEmployeesSuccessfully() = runTest {
+        val instance = RetrofitInstance.getRetrofitInstance(Constants.BASE_EMPLOYEES_URL)?.create(
+            EmployeeDataSource::class.java
+        )
+        if (instance != null) employeeRepoImpl = EmployeeRepoImpl(instance, Constants.EMPLOYEES_JSON_TYPE)
+        when (val response = employeeRepoImpl.getEmployees()) {
+            is ResultWrapper.Success -> {
+                employeeList = response.value?.value?.employees as ArrayList<Employee>
+            }
+        }
         val actual = employeeList.size
-        val expected = 1
-        assertFalse(actual < expected)
+        assert(actual > 0)
+
+    }
+
+    @Test
+    fun getGenericErrorWithMalformedData() = runTest {
+        val instance = RetrofitInstance.getRetrofitInstance(Constants.BASE_EMPLOYEES_URL)?.create(
+            EmployeeDataSource::class.java
+        )
+        if (instance != null) employeeRepoImpl = EmployeeRepoImpl(instance, Constants.MALFORMED_EMPLOYEES_JSON_TYPE)
+        val response = safeApiCall(Dispatchers.Main) { runBlocking { employeeRepoImpl.getEmployees() } as StateFlow<*> }
+        assertEquals(ResultWrapper.GenericError(), response)
     }
 
     @After
